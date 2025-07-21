279f9d0c08322e5ce339adb0144d197c98ef67bd
Márcio Pires Barboza <mpbmarcio@hotmail.com>
2025-07-21 17:27:59 -0300

    feat(RespostaResponseDTO): adiciona construtor para conversão direta de entidade Resposta

--- src/main/java/br/com/mpb/forumhub/dto/response/RespostaResponseDTO.java
+++ src/main/java/br/com/mpb/forumhub/dto/response/RespostaResponseDTO.java
@@ -1,10 +1,21 @@
 package br.com.mpb.forumhub.dto.response;
 
+import br.com.mpb.forumhub.model.Resposta;
+
 import java.time.LocalDateTime;
 
 public record RespostaResponseDTO(Long id,
                                   String mensagem,
                                   LocalDateTime dataInc,
                                   UsuarioResponseDTO autor) {
+    public RespostaResponseDTO(Resposta resposta) {
+        this(resposta.getId(),
+                resposta.getMensagem(),
+                resposta.getDataInc(),
+                new UsuarioResponseDTO(
+                        resposta.getAutor().getId(),
+                        resposta.getAutor().getNome(),
+                        resposta.getAutor().getEmail()));
+    }
 }
 
